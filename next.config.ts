e0761aeba668ba7a0fe60f42ef44e5d0b4c1bf1a
t0mmyqwe <luckijartem@gmail.com>
2025-05-24 17:58:52 +0300

    change next-config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     devIndicators: {
         buildActivity: false,
-        position: "bottom-right"
     },
     transpilePackages: ['three']
 };
